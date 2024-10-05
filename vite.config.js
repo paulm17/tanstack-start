ab5a426f07532f54fedf78a892a9b92e8a2d8d64
Manuel Schiller <manuel.schiller@caligano.de>
2024-10-05 22:14:13 +0200

    fix pigment usage
    1. configure vite plugin in app.config.ts
    2. pin babel version to a working one, see https://github.com/mui/pigment-css/issues/257#issuecomment-2392175633

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { pigment } from "@pigment-css/vite-plugin";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [pigment(), TanStackRouterVite(), react()],
-});
